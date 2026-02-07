c7cebe184ab1c4c7bb6527fac29a148bf755d8fe
Alessandro Tognan <83232805+aletgn@users.noreply.github.com>
2026-02-07 14:05:11 +0100

    Change license from GPLv3 to MIT in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT license",
         "Operating System :: OS Independent",
     ],
     
